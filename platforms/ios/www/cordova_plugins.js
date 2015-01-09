11f431252c07bfb3c6be87f7abdb9b617e82cf45
Arjun TR <arjun200034@gmail.com>
2015-01-09 01:38:31 -0800

    fixing the phonegap build

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,52 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/emailcomposer/EmailComposer.js",
+        "id": "emailcomposer.EmailComposer",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "emailcomposer": "2.0.2",
+    "org.apache.cordova.dialogs": "0.2.11",
+    "org.apache.cordova.geolocation": "0.3.11"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/ios/www/js/questionnaireAnswers.js
@@ -0,0 +1,33 @@
+
+/* get the answers from the medical questionnaire after the submit button is pressed*/
+
+$(function  (){
+  
+  var answers = [];
+  var arrayJSON = [];
+  var temp;
+  var numOfQuestions = 15;
+  var i;
+  var radioPrefixes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "B", "C", "D", "E"];
+  
+  $("#submitAnswers").click(function() {
+    for(i=0; i<numOfQuestions; i++){
+       var index = radioPrefixes[i];
+       var inputName = "input[name*=radio-choice-"+index+"]:checked";
+                            
+                            /*set the value to no answer selected if the user does not select an answer*/
+                            if($(inputName).val() == null){
+                                answers[i] = "No Answer Selected";
+                                arrayJSON[i] = "No Answer Selected";
+                            }
+                            else{
+                                answers[i] = $(inputName).val();
+                                arrayJSON[i] = $(inputName).val();
+                            }
+        }
+        
+        /*store the answers into localStorage to be accessed on another page*/
+        localStorage.setItem("arrayValues", JSON.stringify(arrayJSON));
+
+  });
+});
